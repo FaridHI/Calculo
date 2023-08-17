65e144578f85f5eb227b78e8a3e519b5eeb2389b
FaridHI <1121041@senati.pe>
2023-08-17 15:29:43 -0500

    agregue resta

--- CalculoF/src/main/java/com/mycompany/calculof/CalculoF.java
+++ CalculoF/src/main/java/com/mycompany/calculof/CalculoF.java
@@ -3,7 +3,7 @@
  */
 
 package com.mycompany.calculof;
-
+import java.util.Scanner;
 /**
  *
  * @author Alumno
@@ -11,6 +11,18 @@ package com.mycompany.calculof;
 public class CalculoF {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in); 
+        System.out.println("La Resta");
+               System.out.print("INGRESE EL PRIMER NUMERO: ");
+               
+               int numero3 = scanner.nextInt();
+               
+               System.out.print("INGRESE EL SEGUNDO NUMERO: ");
+               int numero4 = scanner.nextInt();
+               
+               int resta = numero3 - numero4;
+                   System.out.println("la resta de " + numero3 + " - " + numero4 + " es: " + resta);
+               System.out.println();  
+               
     }
 }
